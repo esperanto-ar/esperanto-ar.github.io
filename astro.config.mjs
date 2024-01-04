952d3119928b4388a71c9a5ce29345eda4c83836
Ваше Имя <you@example.com>
2024-01-04 15:57:40 -0300

    новая база для сайта

---
+++ astro.config.mjs
@@ -0,0 +1,13 @@
+import { defineConfig } from "astro/config";
+import tailwind from "@astrojs/tailwind";
+
+import preact from "@astrojs/preact";
+
+// https://astro.build/config
+export default defineConfig({
+  server: {
+    port: 3000,
+    host: true
+  },
+  integrations: [tailwind(), preact()]
+});
